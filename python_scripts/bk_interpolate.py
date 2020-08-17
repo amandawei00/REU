75e413a7fbf8725c8111da052b9bc5e3c4a85c6b
Amanda Wei <amandawei00@gmail.com>
2020-08-17 10:01:07 -0700

    fixed bk_interpolate

--- python_scripts/bk_interpolate.py
+++ python_scripts/bk_interpolate.py
@@ -1,25 +1,26 @@
+# sys.path.append("Ogata/python")
+# from FBT import FBT
 import numpy as np
+import pandas as pd
 import scipy.interpolate as interpolate
 import scipy.integrate as intg
-import pandas as pd
-import csv
-import time
-import sys
-import matplotlib.pyplot as plt
-
-#sys.path.append("Ogata/python")
-#from FBT import FBT
 
 class N():
     def __init__(self):
         self.n_ = 400
         self.x0 = 0.02
-        #self.xr1 = np.log(3.e-6)
+        # self.xr1 = np.log(3.e-6)
         self.xr1 = 0.0
-        self.xr2 = np.log(60.e0) # limit of integration in fourier transform calculation
+        self.xr2 = np.log(60.e0)  # limit of integration in fourier transform calculation
 
         # read results.csv file from BK solution to pandas dataframe
-        self.df = pd.read_csv("cresults.csv", sep="\s+")
+        self.df1 = pd.read_csv("cresults.csv", sep="\s+")
+        self.df2 = pd.read_csv("results-f.csv", sep="\s+")
+
+        self.df1.columns = ['kuta', 'y', 'vr', 'vfr', 'prev']
+        self.df2.columns = ['kuta', 'y', 'vr', 'vfr', 'prev']
+
+        self.df = pd.concat([self.df1, self.df2])
         self.df = self.df.drop(self.df[self.df.kuta == "kuta"].index)
 
         # converting dataframe element types
@@ -28,47 +29,41 @@ class N():
         self.df["vr"] = self.df["vr"].astype('float64')
         self.df["vfr"] = self.df["vfr"].astype('float64')
 
-        self.r = np.unique(self.df.vr.to_numpy(dtype = 'float64'))
-#        self.y = self.df.y.to_numpy(dtype = 'float32')
-#        self.n = self.df.vfr.to_numpy(dtype = 'float64')
-        
-        
+        self.r = np.unique(self.df.vr.to_numpy(dtype='float64'))
+        #        self.y = self.df.y.to_numpy(dtype = 'float32')
+        #        self.n = self.df.vfr.to_numpy(dtype = 'float64')
+
         self.interp_y = []  # to be filled by interpolated functions over r at some fixed y
-        for i in np.arange(0.0, 30.0, 0.1):
-            sub_ = self.df.loc[(self.df['kuta'] == 4) & (self.df['y'] == i)]
+        for i in np.arange(-4.8, 30.0, 0.1):
+            sub_ = self.df.loc[(self.df['kuta'] == 4) & (self.df['y'] == i.round(decimals=1))]
             x_ = np.concatenate(sub_[['vr']].to_numpy(), axis=0)
             y_ = np.concatenate(sub_[['vfr']].to_numpy(), axis=0)
 
-            self.interp_y.append(interpolate.CubicSpline(x_,y_))
-
+            self.interp_y.append(interpolate.CubicSpline(x_, y_))
 
         self.interp_r = []  # to be filled by interpolated functions over y at some fixed r
         for r_ in self.r:
-            sub_ = self.df.loc[(self.df['kuta'] == 4) & (self.df['vr'] == r_)]
+            sub_ = self.df.loc[(self.df['kuta'] == 4) & (self.df['vr'] == r_)].sort_values(by='y')
+            # sub_ = sub_.sort_values(by='y')
             x_ = np.concatenate(sub_[['y']].to_numpy(), axis=0)
             y_ = np.concatenate(sub_[['vfr']].to_numpy(), axis=0)
-
             self.interp_r.append(interpolate.CubicSpline(x_, y_))
 
+    def bk_f(self, y):  # fundamental representation
 
-    def bk_f(self,y): # fundamental representation
-
-#        t1 = time.time()
-        rap = np.arange(0.0, 30.0, 0.1)
+        #        t1 = time.time()
+        rap = np.arange(-4.8, 30.0, 0.1)
         # search file to see if interpolation already exists !!!
-        
+
         if y in rap:
             i = np.where(rap == y)[0][0]
-#            print("bk_f time: " + str(time.time()-t1))
             return self.interp_y[i]
         else:
             a = np.zeros(len(self.r))
             for i in range(len(a)):
                 f = self.interp_r[i]
                 a[i] = f(y)
-#            print("bk_f time: " + str(time.time()-t1))
             return interpolate.CubicSpline(self.r, a)
-
 
         # write interpolated object to file !!!
 
@@ -76,26 +71,27 @@ class N():
         f = self.bk_f(y)
         x = self.r
 
-        g = 2*f(x) - np.power(f(x),2)
+        g = 2 * f(x) - np.power(f(x), 2)
         return interpolate.CubicSpline(self.r, g)
 
-    def udg_f(self, x,  k):
-        f = self.bk_f(np.log(self.x0/x))
-        integrand = lambda r: (1-f(r))*self.bessel(k*r,0)
-        return 2*np.pi*intg.quad(integrand, self.xr1,self.xr2, epsabs=1.e-4)[0]
+    def udg_f(self, x, k):
+        f = self.bk_f(np.log(self.x0 / x))
+        integrand = lambda r: (1 - f(r)) * self.bessel(k * r, 0)
+        return 2 * np.pi * intg.quad(integrand, self.xr1, self.xr2, epsabs=1.e-4)[0]
 
     def udg_a(self, x, k):
-        f = self.bk_a(np.log(self.x0/x))
-        integrand = lambda r_: (1 - f(r_))*self.bessel(k*r_,0)
-        return 2*np.pi*intg.quad(integrand, self.xr1, self.xr2, epsabs=1.e-4)[0]
+        f = self.bk_a(np.log(self.x0 / x))
+        integrand = lambda r_: (1 - f(r_)) * self.bessel(k * r_, 0)
+        return 2 * np.pi * intg.quad(integrand, self.xr1, self.xr2, epsabs=1.e-4)[0]
 
     def bessel(self, x, alpha):
-        f = lambda t: np.cos(alpha*t - x*np.sin(t))
-        return (1/np.pi)*intg.quad(f,0,np.pi, epsabs=1.e-3)[0]
+        f = lambda t: np.cos(alpha * t - x * np.sin(t))
+        return (1 / np.pi) * intg.quad(f, 0, np.pi, epsabs=1.e-3)[0]
 
     # def bessel_intg(self,k):
     #    f = lambda u: (u/np.power(k,2)) * self.bessel(u,0)
     #    return intg.quad(f,0.0,np.inf)
+
 
 # end of class
 
@@ -105,17 +101,18 @@ if __name__ == "__main__":
     k = 71.1797503982
     x0 = 0.02
 
-    x_range = np.arange(0.1,1.0,0.1)
-    integrand = lambda x_,r_: (1-n.bk_f(np.log(x0/x_)))*n.bessel(k*r_,0)
+    n.bk_f(-1.0)
+    # x_range = np.arange(0.1,1.0,0.1)
+    # integrand = lambda x_,r_: (1-n.bk_f(np.log(x0/x_)))*n.bessel(k*r_,0)
 
 #    r_range = np.arange(n.xr1, n.xr2, 0.1)
 
 #    for i in range(len(x_range)):
 #        x = x_range[i]
-        
+
 #        y_ = [integrand(r,x) for r in r_range]
 #        plt.plot(r_range,y_)
 
 #    plt.show()
 
-    print(n.udg_f(x,k))
+# print(n.udg_f(x,k))
